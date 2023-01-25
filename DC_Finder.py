d69a22b8fe7b079bedb1af20912646b4f0910c24
the-jcksn <101604447+the-jcksn@users.noreply.github.com>
2023-01-25 14:50:09 +0000

    Update DC_Finder.py

--- DC_Finder.py
+++ DC_Finder.py
@@ -7,11 +7,17 @@ import subprocess
 #required arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', default='none', dest='network', help='provide the CIDR network to scan (eg 10.0.0.1/24)', type=str)
+parser.add_argument('-l', default='none', dest='network_list', help='provide a list of CIDR networks to scan', type=str)
 args = parser.parse_args()
 
 #check required args completed
-if args.network == 'none':
-        print(colored('\n[!] Network range not provided with \'-n\'.\n\nQuitting.','red'))
+if args.network == 'none' and args.network_list == 'none':
+        print(colored('\n[!] Network range or list not provided with \'-n\' or \'-l\'.\n\nQuitting.','red'))
+        quit()
+
+#check if too many args completed:
+if args.network != 'none' and args.network_list != 'none':
+        print(colored('\n[!] List and network provided! Dont be greedy, only use one...\n\nQuitting.','red'))
         quit()
 
 #check if sudo was used
@@ -26,31 +32,43 @@ def cmdline(command):
         terminal_output = subprocess.getoutput(command)
         return(terminal_output)
 
-printbanner()
-print('\nScanning',colored(args.network,'blue'),'for the Domain Controllers')
-printbanner()
-
-#set the nmap command
-scan_cmd = 'nmap -sS -p 53,88,139,389 --open '+args.network
-
-#run the scan
-initial_scan = cmdline(scan_cmd)
-
-#split the scan into individual IPs
-individual_ips = initial_scan.split('MAC Address')
-
-#check if all required ports are open and add to list of DCs:
+networks = []
+if args.network != 'none':
+        networks.append(args.network)
+if args.network_list != 'none':
+        with open(args.network_list,'r') as network_list:
+                for line in network_list:
+                        networks.append(line)
 domain_controllers = []
-for ip in individual_ips:
-        if '53' in ip:
-                if '88' in ip:
-                        if '139' in ip:
-                                if '389' in ip:
-                                        dc_name = ip.split('Nmap scan report for ')[1]
-                                        dc_name = dc_name.split('Host is up')[0]
-                                        print(colored('\nThis is likely a DC:\n\n','blue'),dc_name)
-                                        printbanner()
-                                        domain_controllers.append(dc_name)
+printbanner()
+for line in networks:
+        if args.network_list != 'none':
+                print('\nScanning',colored(line[:-1],'blue'),'for the Domain Controllers')
+                printbanner()
+        elif args.network != 'none':
+                print('\nScanning',colored(line),'blue'),'for the Domain Controllers')
+                printbanner()
+
+        #set the nmap command
+        scan_cmd = 'nmap -sS -p 53,88,139,389 --open '+line
+
+        #run the scan
+        initial_scan = cmdline(scan_cmd)
+
+        #split the scan into individual IPs
+        individual_ips = initial_scan.split('MAC Address')
+
+        #check if all required ports are open and add to list of DCs:
+        for ip in individual_ips:
+                if '53' in ip:
+                        if '88' in ip:
+                                if '139' in ip:
+                                        if '389' in ip:
+                                                dc_name = ip.split('Nmap scan report for ')[1]
+                                                dc_name = dc_name.split('Host is up')[0]
+                                                print(colored('\nThis is likely a DC:\n\n','blue'),dc_name)
+                                                printbanner()
+                                                domain_controllers.append(dc_name)
 
 if len(domain_controllers) < 1:
         print('\nWe didn\'t find shit. Probably your fault...')
